06e643f6fd3f863ac2be6b4dcd89971426520027
William <william.j.schober.reyes@gmail.com>
2019-02-19 19:15:49 -0500

    Updates

---
+++ precache-manifest.96c54a934c271db69fd49215434089b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5955a3ac404b41da551",
+    "url": "/Towers-of-Hanoi/static/js/runtime~main.c5955a3a.js"
+  },
+  {
+    "revision": "ea76510d7abc34decd7c",
+    "url": "/Towers-of-Hanoi/static/js/main.ea76510d.chunk.js"
+  },
+  {
+    "revision": "1c716682272d2ca3876b",
+    "url": "/Towers-of-Hanoi/static/js/2.1c716682.chunk.js"
+  },
+  {
+    "revision": "ea76510d7abc34decd7c",
+    "url": "/Towers-of-Hanoi/static/css/main.e9bbdbac.chunk.css"
+  },
+  {
+    "revision": "6d7940f4563dcc13a477382e54612a14",
+    "url": "/Towers-of-Hanoi/index.html"
+  }
+];
